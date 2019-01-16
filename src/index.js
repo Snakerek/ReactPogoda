36e44af27f752e309cfe098deb2d4e72f51bf35d
Kacper Zieliński <zielinski.kac@gmail.com>
2019-01-16 10:48:49 +0100

    Initial Commit. Utworzenie struktury podstawowej.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+// Import the React and ReactDOM libraries
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Utworzenie komponentu React
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+	return <div>HELLO</div>;
+};
+//Wyświetlenie komponentu na ekranie
+
+ReactDOM.render(<App />, document.querySelector('#root'));
