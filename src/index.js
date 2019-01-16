b04e4a3a1261efc22e634d08e347a9bc97da800d
Kacper Zieliński <zielinski.kac@gmail.com>
2019-01-16 11:22:43 +0100

    Zdefiniowanie konstruktora

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import ReactDOM from 'react-dom';
 // Utworzenie komponentu React
 
 class App extends React.Component {
+	//konstruktor obiektu. Za każdym razem należy odwołać się do props nadrzędnych tak jak poniżej
+	constructor(props) {
+		super(props);
+
+		this.state = { lat: null };
+	}
+	// render() jest funkcją konieczną dla Reacta do zadziałania
 	render() {
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => console.log(position), //w przypadku prawidłowego pobrania lokalizacji
