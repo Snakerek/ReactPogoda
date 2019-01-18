1fc354866a963e6500b4be14dfd23cfae036bbcd
Kacper Zieliński <zielinski.kac@gmail.com>
2019-01-18 10:45:06 +0100

    Pory roku na podstawie szerokości geograficznej i dodanie  ikon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,9 +1,25 @@
 import React from 'react';
 //
 
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? 'Lato' : 'Zima';
+	} else {
+		return lat > 0 ? 'Zima' : 'Lato';
+	}
+};
+
 const SeasonDisplay = (props) => {
-	console.log(props.lat);
-	return <div>Season</div>;
+	const season = getSeason(props.lat, new Date().getMonth());
+	const text = season === 'Zima' ? 'Jest zimno' : 'Jest ciepło';
+	const icon = season === 'Zima' ? 'snowflake' : 'sun';
+	return (
+		<div>
+			<i className={`${icon} icon`} />
+			<h1>{text}</h1>
+			<i className={`${icon} icon`} />
+		</div>
+	);
 };
 
 export default SeasonDisplay;
